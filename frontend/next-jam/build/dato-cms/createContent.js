907573ad0cc24df79a1082b7625528279365e492
Johnny Kristensen <johnny.kristensen@akqa.com>
2019-12-31 14:11:36 +0100

    DatoCMS build dev

--- frontend/next-jam/build/dato-cms/createContent.js
+++ frontend/next-jam/build/dato-cms/createContent.js
@@ -3,20 +3,67 @@ const traverse = require("./traverse");
 const createPage = require("./createPage");
 
 const createContent = (dato, root, i18n) => {
-    const collectionTypes = Object.entries(dato.collectionsByType).map(type => type[0]);
+    console.time("build collections");
 
-    collectionTypes.forEach(type => {
-        const records = dato[type];
+    const collectionsByTypes = dato.collectionsByType;
 
-        if (type === "frontPage") return;
+    let content = [];
+    let siteMap = [];
+    for (let key in collectionsByTypes) {
+        if(!collectionsByTypes.hasOwnProperty(key)) continue;
 
-        traverse(
-            records,
-            constants.rootPath,
-            root,
-            (page, path, root) => createPage(page, path, root)
-          );
-    });
+        const items = collectionsByTypes[key]
+
+        if (!Array.isArray(items)) continue;
+
+        items.forEach(item => {
+            const entity = item.entity;
+
+            const url = buildUrl(item, items);
+
+            const baseObject = {
+                id: entity.payload.id,
+                type: item.itemType.name,
+                url
+            };
+
+            siteMap.push(baseObject);
+        });
+    }
+
+    // collectionTypes.forEach(type => {
+    //     const records = dato[type];
+
+    //     if (type === "frontPage") return;
+
+    //     traverse(
+    //         records,
+    //         constants.rootPath,
+    //         root,
+    //         (page, path, root) => createPage(page, path, root)
+    //       );
+    // });
+    // console.log(siteMap)
+    console.timeEnd("build collections")
+}
+
+const buildUrl = (item, itemList) => {
+    const entity = item.entity;
+    let url = `/${item.slug}/`
+    let parentId = entity.parentId;    
+
+    if (!parentId) return url;
+
+    while (parentId) {
+        const parent = itemList.filter(x => x.entity.payload.id === parentId)[0];
+
+        if (parent) {
+            url = `/${parent.slug}${url}`;
+            parentId = parent.entity.parentId
+        };
+    }
+
+    return url;
 }
 
 module.exports = createContent;
