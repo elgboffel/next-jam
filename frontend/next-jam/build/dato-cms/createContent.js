9d1524ee7e09db35642da8e51965196a5604dd25
Johnny Kristensen <johnny.kristensen@akqa.com>
2020-01-04 12:04:58 +0100

    Limiting to map

--- frontend/next-jam/build/dato-cms/createContent.js
+++ frontend/next-jam/build/dato-cms/createContent.js
@@ -47,7 +47,7 @@ const buildInfo = (dato) => {
         console.log("################### Incoming Hook Body ###################");
         console.log(process.env.INCOMING_HOOK_BODY);
         console.log("################### Incoming Hook Body End ###################");
-        console.log("Item: ", dato.find(json.entity_id).toMap());
+        console.log("Item: ", dato.find(json.entity_id).toMap(1));
     }
 }
 
